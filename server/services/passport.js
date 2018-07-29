e4fc3edfd36bca760b5f5fc02d41bd6aecaed296
Dodo <dmukunzi0@gmail.com>
2018-07-29 11:11:42 +0200

    feat(*): implement google oauth with passport

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,21 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User');
+
+// User encoding
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// User decoding
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +25,16 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token: ', accessToken);
-      console.log('refresh token: ', refreshToken);
-      console.log('profile: ', profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // Already signed up
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
